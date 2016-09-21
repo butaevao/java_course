0830e652af3d2125aeae7a9c55b0e8e9ef1015cd
butaevao <butaevao@gmail.com>
2016-09-22 01:06:26 +0300

    Готовое для проверки Задание2_v.2

--- sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
@@ -3,12 +3,10 @@ package ru.stqa.course.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-
-
-	  Point p1 = new Point(4, -5);
-	  Point p2 = new Point(7, -1);
+		Point p1 = new Point(4, -5);
+		Point p2 = new Point(7, -1);
 		System.out.println("Вычисление расстояния между двумя точками с использованием функции:");
-    System.out.println("Расстояние между точками p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+		System.out.println("Расстояние между точками p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
 
 		Point p3 = new Point(4, -5);
 		Point p4 = new Point(4, -5);
