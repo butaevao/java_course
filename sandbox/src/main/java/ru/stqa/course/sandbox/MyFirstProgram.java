eef58d7e5bd580fbf9e99bb54772bc8ef57f9a08
butaevao <butaevao@gmail.com>
2016-09-22 00:56:34 +0300

    Готовое для проверки Задание2

--- sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/MyFirstProgram.java
@@ -3,5 +3,20 @@ package ru.stqa.course.sandbox;
 public class MyFirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
+
+
+	  Point p1 = new Point(4, -5);
+	  Point p2 = new Point(7, -1);
+		System.out.println("Вычисление расстояния между двумя точками с использованием функции:");
+    System.out.println("Расстояние между точками p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+
+		Point p3 = new Point(4, -5);
+		Point p4 = new Point(4, -5);
+		System.out.println("Вычисление расстояния между двумя точками с использованием метода:");
+		System.out.println("Расстояние между точками p3(" + p3.x + "," + p3.y + ") и p4(" + p4.x + "," + p4.y + ") = " + p4.newDistance(p3));
+}
+
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p2.x - p1.x), 2) + (Math.pow((p2.y - p1.y), 2)));
 	}
 }
--- sandbox/src/main/java/ru/stqa/course/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/course/sandbox/Point.java
@@ -1,24 +1,19 @@
 package ru.stqa.course.sandbox;
 
 /**
- * Created by Оля on 20.09.2016.
+ * Created by Оля on 22.09.2016.
  */
 public class Point {
 
-  private double x;
-  private double y;
+  public double x;
+  public double y;
 
-  public static void main(String[] args) {
-    Point p1 = new Point();
-    Point p2 = new Point();
-    p1.x = 4;
-    p1.y = -5;
-    p2.x = 7;
-    p2.y = -1;
-    System.out.println("Расстояние между точками p1(" + p1.x + "," + p1.y + ") и p2(" + p2.x + "," + p2.y + ") = " + distance(p1, p2));
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
   }
 
-  private static double distance(Point p1, Point p2) {
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + (Math.pow((p2.y - p1.y), 2)));
+  public double newDistance(Point p3) {
+    return Math.sqrt(Math.pow((x - p3.x), 2) + (Math.pow((y - p3.y), 2)));
   }
 }
