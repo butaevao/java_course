8af89a946f28c85a0361a1b80dc94a7f37baab77
butaevao <butaevao@gmail.com>
2016-10-01 22:59:46 +0300

    Создан класс BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ApplicationManager.java
@@ -1,6 +1,5 @@
 package ru.stqa.course.addressbook.appmanager;
 
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -16,14 +15,6 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
 
   public void init() {
     wd = new FirefoxDriver();
--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/NavigationHelper.java
@@ -6,22 +6,21 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by Оля on 01.10.2016.
  */
-public class NavigationHelper {
-  private FirefoxDriver wd;
+public class NavigationHelper extends HelperBase {
 
   public NavigationHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void gotoGroupPage() {
-    wd.findElement(By.linkText("groups")).click();
+    click(By.linkText("groups"));
   }
 
   public void gotoAddNewPage() {
-    wd.findElement(By.linkText("add new")).click();
+    click(By.linkText("add new"));
   }
 
   public void goToHomePage() {
-    wd.findElement(By.linkText("home")).click();
+    click(By.linkText("home"));
   }
 }
