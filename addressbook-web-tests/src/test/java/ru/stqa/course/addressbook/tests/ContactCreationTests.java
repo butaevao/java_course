f901c721d2d5aaea65054968c4fa5e8181cc459f
butaevao <butaevao@gmail.com>
2016-10-08 19:40:47 +0300

    Использование лямбда-выражений

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/tests/ContactCreationTests.java
@@ -3,8 +3,6 @@ package ru.stqa.course.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.course.addressbook.model.ContactData;
-import ru.stqa.course.addressbook.model.GroupData;
-
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +19,14 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after) {
-      if (c.getId() > max) {
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    //int max = 0;
+    //for (ContactData c : after) {
+    //  if (c.getId() > max) {
+    //    max = c.getId();
+    //  }
+    //}
+
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
