30473749e369c8fee2787c80d032eb1fb66846c9
butaevao <butaevao@gmail.com>
2016-10-08 19:15:00 +0300

    Использование поиска максимального элемента в коллекции на примере тестов создания групп/контактов

--- addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/course/addressbook/appmanager/ContactHelper.java
@@ -87,7 +87,7 @@ public class ContactHelper extends HelperBase {
     List<ContactData> contacts = new ArrayList<ContactData>();
     List<WebElement> elements = wd.findElements(By.name("entry"));
     for (WebElement element : elements) {
-      String id = element.findElement(By.tagName("input")).getAttribute("id");
+      int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("id"));
       String name = element.findElement(By.xpath("./td[3]")).getText();
       String surname = element.findElement(By.xpath("./td[2]")).getText();
       String address = element.findElement(By.xpath("./td[4]")).getText();
